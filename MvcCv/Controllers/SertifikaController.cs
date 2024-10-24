7e24f5ea0868ffb27e89ff352a31917ae0cec4a7
Hüseyin Kurban <huseyinkurban03@gmail.com>
2024-10-24 19:59:44 +0300

    Add project files.

---
+++ MvcCv/Controllers/SertifikaController.cs
@@ -0,0 +1,56 @@
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class SertifikaController : Controller
+    {
+        // GET: Sertifika
+        SertifikaRepository repo = new SertifikaRepository();
+        public ActionResult Index()
+        {
+            var degerler = repo.List();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult SertifikaGetir(int id)
+        {
+            var sertifika = repo.Find(x => x.ID == id);
+            ViewBag.Sil = id;
+            return View(sertifika);
+        }
+        [HttpPost]
+        public ActionResult SertifikaGetir(TblSertifikalarim t)
+        {
+            var sertifika = repo.Find(x => x.ID == t.ID);
+            sertifika.Aciklama = t.Aciklama;
+            sertifika.Tarih = t.Tarih;
+            repo.TUpdate(sertifika);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult YeniSertifika()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniSertifika(TblSertifikalarim t)
+        {
+            repo.TAdd(t);
+            return RedirectToAction("Index");
+        }
+        public ActionResult SertifikaSil(int id)
+        {
+            var sertifika = repo.Find(x => x.ID == id);
+            
+            repo.TDelete(sertifika);
+            return RedirectToAction("Index");
+        }
+    }
+}
